980dd73d5b7b5e3bd5cb8ce7b285c725a34a0fe7
prasanth8 <prasanth871997@gmail.com>
2016-12-21 21:42:56 -0800

    Add files via upload

---
+++ beg1nnumber.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.util.*;
+public class testconfirm {
+    public static void main(String args[]) 
+    { int a,sum=0;
+    Scanner obj=new Scanner(System.in);
+    System.out.println("Enter the n vlaue");
+    a=obj.nextInt();
+    for(int i=0;i<=a;i++){
+    	sum=sum+i;
+    }System.out.println(sum);
+        } 
+} 
