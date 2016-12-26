71bb9a2cc522965862d93c184898b20aa5982751
prasanth8 <prasanth871997@gmail.com>
2016-12-25 20:23:47 -0800

    Add files via upload

---
+++ beg39bigamong10no.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class greatamongthreeno {
+public static void main(String[] args) {
+	int[] a=new int[100];
+	int greater=0;
+	System.out.println("Enter the three numbers");
+	Scanner obj=new Scanner(System.in);
+	for(int i=0;i<10;i++)
+	{
+		a[i]=obj.nextInt();
+		if(a[i]>greater)
+			greater=a[i];
+	}System.out.println(greater);
+}
+}
