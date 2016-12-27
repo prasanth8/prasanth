7659bfffa1fd9eebfbfee87c2772cf49b21320a9
prasanth8 <prasanth871997@gmail.com>
2016-12-27 10:28:37 +0530

    Add files via upload

---
+++ hunter132printstar.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class hunter132printstar {
+public static void main(String[] args) {
+	int a;
+	System.out.println("Enter the number to print star");
+	Scanner obj=new Scanner(System.in);
+	a=obj.nextInt();
+	for(int i=a;i>0;i--){
+		for(int j=a;j>0;j--)
+			System.out.print("*");
+		System.out.print(" ");
+		for(int k=a;k>0;k--)
+			System.out.print("*");
+		System.out.println();
+		a--;
+}}
+}
