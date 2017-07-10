a33023bcb8ac56c73306edd3deedc8282ae8f053
prasanth8 <prasanth871997@gmail.com>
2017-07-10 18:02:45 +0530

    Add files via upload

---
+++ hunter48substring.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.*;
+
+public class hunter48substring {
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter the First String");
+	String s1=in.next();
+	System.out.println("Enter the second string");
+	String s2=in.next();
+	System.out.println(s1.indexOf(s2));
+	}
+}
