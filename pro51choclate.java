2a44df575143f69ba4bb96f6f6ce692ea5c3221b
prasanth8 <prasanth871997@gmail.com>
2017-07-16 10:09:06 +0530

    Add files via upload

---
+++ pro51choclate.java
@@ -0,0 +1,15 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class pro51choclate {
+
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+		System.out.println("Enter the M and N value");
+	int m=in.nextInt();
+	int n=in.nextInt();
+		System.out.println((m*n)-1);
+		
+	}
+	}
